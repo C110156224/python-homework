a72cd02d40724e25783e607ef2e988e59a6f836e
吳Wu <c110156224@nkust.edu.tw>
2022-04-03 20:48:26 +0800

    39

---
+++ 39.py
@@ -0,0 +1,31 @@
+n = int(input("輸入整數"))
+if n%2>0:
+    for i in range(n+1,0,-2):
+        for j in range(i):
+            print(' ',end='')
+        for k in range(n+2-i):
+            print('x ',end='')
+        print(
+
+        )
+    for x in range(n-1,0,-2):
+        for y in range(n+2-x+1):
+            print(" ",end="")
+        for z in range(x,1,-1):
+            print("x ",end="")
+        print()
+if n%2<1:
+    for i in range(n+2,0,-2):
+        for j in range(i):
+            print(' ',end='')
+        for k in range(n-i+1):
+            print('x ',end='')
+        print(
+
+        )
+    for x in range(n-1,0,-2):
+        for y in range(n+2-x+1):
+            print(" ",end="")
+        for z in range(x-2+2,2,-1):
+            print("x ",end="")
+        print()
