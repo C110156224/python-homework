1015bd51bdafb8b1f60374e5c93874bf522d43f5
吳Wu <c110156224@nkust.edu.tw>
2022-04-09 01:27:17 +0800

    6

---
+++ 6.py
@@ -0,0 +1,19 @@
+a = input("輸入1~7個數字字串").split(",")
+# a = [1,3,9,5,7,1]
+x = 0
+a2 = []
+a3 = []
+for i in range(len(a)):
+    for j in range(len(a)-1-i):
+        if a[j+1] > a[j]:
+            x = a[j]
+            a[j] = a[j+1]
+            a[j+1] = x
+for m in range(len(a)):
+    a2.append(str(a[m]))
+for n in range(len(a)-1,-1,-1):
+    a3.append(str(a[n]))
+max = int("".join(a2))
+min = int("".join(a3))
+print("最大值數列與最小值數列差值為:",max-min)
+
