93bf67c049053f83297d5ed559b2184795058418
吳Wu <c110156224@nkust.edu.tw>
2022-04-13 19:09:07 +0800

    13

---
+++ 13.py
@@ -0,0 +1,9 @@
+word = ("12345")
+if str(word) == str(word)[::-1]:
+    print("yes")
+else:
+    print("no")
+# if str(word) == "".join(reversed(word)) :
+#     print("yes")
+# else:
+#     print("no")
