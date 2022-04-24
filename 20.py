33162b3b7d13f3f4453f3c6b021731473562392f
吳Wu <c110156224@nkust.edu.tw>
2022-04-24 22:36:49 +0800

    20

---
+++ 20.py
@@ -0,0 +1,19 @@
+a = 3
+s = []
+for i in range(a):
+    s.append(input("第"+str(i+1)+"組").split(" "))
+# s = [["5","3"],["6","4"],["7","6"]]
+c = []
+for j in range(a):
+    b = s[j]
+    n = []
+    for k in range(2):
+        n.append(int(b[k]))
+    c.append(n)
+for x in range(a):
+    x1 = c[x]
+    big = x1[0] * 250
+    small = x1[1] * 175
+    print("第",x+1,"組應收費用為:",big+small)
+    
+        
