36561fffdb347d0e33ebb790317c9ee5433cdbdc
吳Wu <c110156224@nkust.edu.tw>
2022-05-07 21:49:23 +0800

    41

--- 41.py
+++ 41.py
@@ -3,7 +3,7 @@ t = int(input("小名搭幾次電梯:"))
 floor = [1]
 for i in range(t):
     floor.append(int(input("搭到幾樓")))
-print(floor)
+# print(floor)
 # floor = [1,2,3,5,3,1]
 price = 0
 for j in range(t):
@@ -11,4 +11,4 @@ for j in range(t):
         price += (floor[j+1]-floor[j])*20
     elif floor[j] > floor[j+1]:
         price += (floor[j]-floor[j+1])*10
-print(price,"元")
+print(str(price)+"元")
