785a3d869070cc109fecc87030c444b058c1bcf6
吳Wu <c110156224@nkust.edu.tw>
2022-05-07 21:47:02 +0800

    38

--- 38.py
+++ 38.py
@@ -8,6 +8,6 @@ x = 0
 for j in range(n):
     if maybe[j] % 9 == 0 or maybe[j] % 11 == 0:
         x +=1
-        print("第",j+1,"個點:",maybe[j])
+        print("第"+str(j+1)+"個點:"+str(maybe[j]))
 if x == 0:
     print(0)
