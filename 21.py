da221acecb56075b3b15991461fd040243b1a7a5
吳Wu <c110156224@nkust.edu.tw>
2022-05-07 21:10:31 +0800

    21

--- 21.py
+++ 21.py
@@ -2,4 +2,4 @@ a =  [[123,456,789,321,654],["Tom","Cat","Nana","Lim","Won"],["DTGD","CSIE","ASI
 id = int(input("請輸入一個學號"))
 for i in range(len(a[0])):
     if id == a[0][i]:
-        print(a[0][i],a[1][i],a[2][i])
+        print("學生資料為:"+str(a[0][i]),a[1][i],a[2][i])
