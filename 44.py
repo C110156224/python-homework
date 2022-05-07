bbdcff9e47f838dee56d683b1deb57b741799f32
吳Wu <c110156224@nkust.edu.tw>
2022-05-07 19:42:04 +0800

    44

---
+++ 44.py
@@ -0,0 +1,11 @@
+T = int(input("任教幾班:"))
+# T = 5
+listT = []
+for i in range(T):
+    listT.append(int(input("每班人數:")))
+# listT = [42,39,41,43,30]
+max = 0
+for j in range(T):
+    if listT[j] > max:
+        max = listT[j]
+print("須購買的電腦數量:",max)
